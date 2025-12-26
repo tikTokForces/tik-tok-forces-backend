13f59138bde8ddc50236764500ee27030d212f26
tikTokForces <projectgithub44@gmail.com>
2025-12-26 19:27:41 +0200

    Fix CORS: allow all methods

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins
     allow_credentials=False,  # Must be False when using wildcard origins
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],  # All HTTP methods
+    allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers including Content-Type, Authorization, etc.
     expose_headers=["*"],  # Expose all headers
     max_age=3600,  # Cache preflight requests for 1 hour
